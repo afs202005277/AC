38af240a776d6766e982fd42a3baafa51bf6ffa0
Pedro Fonseca <pedrojgfonseca@gmail.com>
2023-10-26 15:34:02 +0100

    changed models being used

--- main.py
+++ main.py
@@ -674,7 +674,7 @@ def main():
                 range(1, lag_years_players + 1)] + ['year']
     target = 'EFF'
     trained_models_players = models_train_and_test_players(dataframes_dict['players_teams'], features, target)
-    eff_model = trained_models_players['Lasso Regression']
+    eff_model = trained_models_players['Random Forest Regressor']
     dataframes_dict['players_teams'] = models_predict_future_players(dataframes_dict['players_teams'], features,
                                                                      features_to_be_lagged,
                                                                      eff_model,
@@ -684,7 +684,7 @@ def main():
     target = 'DPR'
 
     trained_models_players = models_train_and_test_players_dpr(dataframes_dict['players_teams'], features, target)
-    dpr_model = trained_models_players['Lasso Regression']
+    dpr_model = trained_models_players['Random Forest Regressor']
 
     dataframes_dict['players_teams'] = models_predict_future_players_dpr(dataframes_dict['players_teams'], features,
                                                                          features_to_be_lagged,
